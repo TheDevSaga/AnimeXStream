326ea2c254fe5c2a21b17c4fcccb5fb846abda41
sagar <thesaagararora@gmail.com>
2023-05-27 14:37:04 +0530

    Remove Test imports

--- app/src/androidTest/java/net/xblacky/animexstream/ui/main/home/di/HomeRepositoryModuleTest.kt
+++ app/src/androidTest/java/net/xblacky/animexstream/ui/main/home/di/HomeRepositoryModuleTest.kt
@@ -1,17 +1,17 @@
 package net.xblacky.animexstream.ui.main.home.di
 
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
-import dagger.hilt.android.testing.BindValue
-import dagger.hilt.testing.TestInstallIn
-import net.xblacky.animexstream.ui.main.home.source.HomeFakeDefaultRepository
-import net.xblacky.animexstream.ui.main.home.source.HomeRepository
-import org.junit.Assert.*
-
-import org.junit.Test
-import javax.inject.Qualifier
+//import dagger.Binds
+//import dagger.Module
+//import dagger.hilt.android.components.ViewModelComponent
+//import dagger.hilt.android.scopes.ViewModelScoped
+//import dagger.hilt.android.testing.BindValue
+//import dagger.hilt.testing.TestInstallIn
+//import net.xblacky.animexstream.ui.main.home.source.HomeFakeDefaultRepository
+//import net.xblacky.animexstream.ui.main.home.source.HomeRepository
+//import org.junit.Assert.*
+//
+//import org.junit.Test
+//import javax.inject.Qualifier
 
 //
 //@Module
